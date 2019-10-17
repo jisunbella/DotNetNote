02d2093601c4b6ff69ad539e4d99b3f54979d0f3
이지선 <easysun93@feelanet.com>
2019-10-17 15:09:20 +0900

    Initialized

---
+++ DotNetNoteSP/DotNetNoteSP/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DotNetNoteSP.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
